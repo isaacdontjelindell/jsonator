b0adba07d536664b198ac8d7f5df16a7d697adfb
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2014-06-11 17:55:12 -0500

    can parse func followed by str (only)

--- parse/grammar.jison
+++ parse/grammar.jison
@@ -24,11 +24,13 @@ expressions
     : e EOF
         { typeof console !== 'undefined' ? console.log($1) : print($1);
           return $1; }
+    | e e EOF
+        { $$ = $1 + $2; }
     ;
 
 e
     : '{{' FUNC '}}'
-        {$$ = $2;}
+        {console.log('func: ' + $2); $$ = $2;}
     | STR
         { console.log('str: ' + $1); $$ = $1;}
     ;
--- parse/grammar.js
+++ parse/grammar.js
@@ -76,7 +76,7 @@ var parser = {trace: function trace() { },
 yy: {},
 symbols_: {"error":2,"expressions":3,"e":4,"EOF":5,"{{":6,"FUNC":7,"}}":8,"STR":9,"$accept":0,"$end":1},
 terminals_: {2:"error",5:"EOF",6:"{{",7:"FUNC",8:"}}",9:"STR"},
-productions_: [0,[3,2],[4,3],[4,1]],
+productions_: [0,[3,2],[3,3],[4,3],[4,1]],
 performAction: function anonymous(yytext, yyleng, yylineno, yy, yystate /* action[1] */, $$ /* vstack */, _$ /* lstack */) {
 /* this == yyval */
 
@@ -85,14 +85,16 @@ switch (yystate) {
 case 1: typeof console !== 'undefined' ? console.log($$[$0-1]) : print($$[$0-1]);
           return $$[$0-1]; 
 break;
-case 2:this.$ = $$[$0-1];
+case 2: this.$ = $$[$0-2] + $$[$0-1]; 
 break;
-case 3: console.log('str: ' + $$[$0]); this.$ = $$[$0];
+case 3:console.log('func: ' + $$[$0-1]); this.$ = $$[$0-1];
+break;
+case 4: console.log('str: ' + $$[$0]); this.$ = $$[$0];
 break;
 }
 },
-table: [{3:1,4:2,6:[1,3],9:[1,4]},{1:[3]},{5:[1,5]},{7:[1,6]},{5:[2,3]},{1:[2,1]},{8:[1,7]},{5:[2,2]}],
-defaultActions: {4:[2,3],5:[2,1],7:[2,2]},
+table: [{3:1,4:2,6:[1,3],9:[1,4]},{1:[3]},{4:6,5:[1,5],6:[1,3],9:[1,4]},{7:[1,7]},{5:[2,4],6:[2,4],9:[2,4]},{1:[2,1]},{5:[1,8]},{8:[1,9]},{1:[2,2]},{5:[2,3],6:[2,3],9:[2,3]}],
+defaultActions: {5:[2,1],8:[2,2]},
 parseError: function parseError(str, hash) {
     if (hash.recoverable) {
         this.trace(str);
