cc202f5a1bf370dd9d1e47034b1647963e440ba7
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2014-06-15 00:04:42 -0500

    post saves schema, get generates data

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var p = require('./parse/parse');
 
 var redis = require('redis');
-var client = redis.createClient();
+    client = redis.createClient();
 
-var app = require('express').express();
+var express = require('express');
+    app = express();
+    cors = require('cors');
 
 var data =
     [
@@ -20,9 +22,37 @@ var data =
     ];
 
 
-app.post
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(app.router);
+
+var id = 0;
+
+app.post('/', function (req, res, next) {
+    var schema = req.body.schema;
+//    console.log(schema);
+
+    client.set(id, schema);
+
+    res.send({id: id});
+    id++
+});
+
+app.get('/:id', function (req, res) {
+    console.log(typeof parseInt(req.params.id));
+    client.get(parseInt(req.params.id), function (err, reply) {
+        if (err != null) { console.log(err);}
+        var schemaObj = JSON.parse(reply);
+
+        res.send(JSON.stringify(p.generate(schemaObj)))
+    })
+});
 
 
+app.listen(8080, function() {
+    console.log('listening...');
+});
 
 
 
